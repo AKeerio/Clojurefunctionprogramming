0b19cc9f3e1f54aa00e8a5800414629931c504d3
Aijaz Ahmed Keerio <aijazkeerio@gmail.com>
2018-04-16 03:09:07 +0100

    Parsed all files

--- tasktwo/src/tasktwo/core.clj
+++ tasktwo/src/tasktwo/core.clj
@@ -14,14 +14,33 @@
 ;Convert the LazySeq to a vector
 (def files-vec (vec (only-files files)))
 
-;Testing on 100 files becuase its taking too long
-(def files2 (take 100 files-vec))
 (def orders [])
-(for [i files2]
+(def purchase-orders [])
+(def shipments [])
+
+(for [i files-vec]
     (if (boolean (re-find #"orders.json" (str i)))
       (def orders (concat orders (parse-stream (clojure.java.io/reader i) true)))
+      ;(println "Not a order")
     )
 )
-(print orders)
-(println "There are" (count (map :id orders)) "unique products")
-(map :delivery-address orders)
+
+(def purchase-orders [])
+(for [i files-vec]
+    (if (boolean (re-find #"purchase-orders.json" (str i)))
+      (def purchase-orders (concat purchase-orders (parse-stream (clojure.java.io/reader i) true)))
+      ;(println "Not a purchase-order")
+    )
+)
+
+(def shipments [])
+(for [i files-vec]
+    (if (boolean (re-find #"shipments.json" (str i)))
+      (def shipments (concat shipments (parse-stream (clojure.java.io/reader i) true)))
+      ;(println "Not a shipments")
+    )
+)
+
+(println "There are" (count (map :id orders)) "unique orders")
+(println "There are" (count (map :id purchase-orders)) "unique purchase-orders")
+(println "There are" (count (map :id shipments)) "unique shipments")
